774c45b90866e23750bff68cf89bc3220a14ce4d
e-emd <dewin.emd@gmail.com>
2020-12-24 19:58:49 -0700

    react router in index and rout paths in app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './components/Nav';
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Route path='/'>
+        <AboutUs />
+      </Route>
+      <Route path='/work'>
+        <OurWork />
+      </Route>
+      <Route path='/contact'>
+        <ContactUs />
+      </Route>
     </div>
   );
 }
